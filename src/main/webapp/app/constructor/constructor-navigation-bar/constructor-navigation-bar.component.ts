0fbcd4dd10859a60406c893110b1779f7065092a
Carlos Madrid <dev.cmadrid@gmail.com>
2020-01-07 16:07:34 -0600

    FEAT: Se modificó estructura para dejar 5 bloques principales en el layout.

---
+++ src/main/webapp/app/constructor/constructor-navigation-bar/constructor-navigation-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-constructor-navigation-bar',
+  templateUrl: './constructor-navigation-bar.component.html',
+  styleUrls: ['./constructor-navigation-bar.component.scss']
+})
+export class ConstructorNavigationBarComponent {
+  constructor() {}
+}
---
+++ src/main/webapp/app/layouts/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-left-sidebar',
+  templateUrl: './left-sidebar.component.html',
+  styleUrls: ['./left-sidebar.component.scss']
+})
+export class LeftSidebarComponent {
+  constructor() {}
+}
