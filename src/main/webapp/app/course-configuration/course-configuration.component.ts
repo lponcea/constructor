556c32af507f22754b1c9eaa537693ba1a35654a
devcmadrid <dev.cmadrid@gmail.com>
2020-02-21 13:08:44 -0600

    FEAT: Se agregaron eventos y efectos a pestañas de pantalla de configuración inicial de libro.

--- src/main/webapp/app/course-configuration/course-configuration.component.ts
+++ src/main/webapp/app/course-configuration/course-configuration.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-configuration.component.scss']
 })
 export class CourseConfigurationComponent {
+  selectedTabIndex = 0;
+  tabs = [
+    {
+      title: 'General',
+      iconClass: 'c-icon-1'
+    },
+    {
+      title: 'Avanzado',
+      iconClass: 'c-icon-2'
+    },
+    {
+      title: 'Recursos',
+      iconClass: 'c-icon-3'
+    }
+  ];
+  markElementStyles = {
+    transform: 'translateX(' + this.selectedTabIndex * 80 + 'px)'
+  };
+
   constructor() {}
 }
