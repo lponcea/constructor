e230870b4fc00b254cfda3b1039518f6b24a3991
devcmadrid <dev.cmadrid@gmail.com>
2020-02-28 17:02:03 -0600

    FEAT: Se agregó funcionalida de obtener video y se eliminó código de ideogular que no se requiere.

--- src/main/webapp/app/constructor/content-blocks/content-block3/content-block3.component.ts
+++ src/main/webapp/app/constructor/content-blocks/content-block3/content-block3.component.ts
@@ -1,20 +1,6 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { IDRMLicenseServer } from 'videogular2/compiled/streaming';
-import { VgDASH } from 'videogular2/compiled/src/streaming/vg-dash/vg-dash';
-import { VgHLS } from 'videogular2/compiled/src/streaming/vg-hls/vg-hls';
-import { VgAPI, BitrateOption } from 'videogular2/compiled/core';
-import { Subscription, timer } from 'rxjs';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FileUploadService } from './../../../services/file-upload.service';
-// import { TimerObservable } from 'rxjs/Observable/TimerObservable';
-
-export interface IMediaStream {
-  type: 'vod' | 'dash' | 'hls';
-  source: string;
-  label: string;
-  token?: string;
-  licenseServers?: IDRMLicenseServer;
-}
 
 @Component({
   selector: 'jhi-content-block3',
@@ -22,87 +8,16 @@ export interface IMediaStream {
   styleUrls: ['./content-block3.component.scss']
 })
 export class ContentBlock3Component implements OnInit {
-  @ViewChild(VgDASH, { static: false })
-  vgDash!: VgDASH;
-  @ViewChild(VgHLS, { static: false })
-  vgHls!: VgHLS;
+  imageUrl: SafeUrl = '';
 
-  currentStream!: IMediaStream;
-  api!: VgAPI;
+  constructor(private fileUploadService: FileUploadService, private sanitizer: DomSanitizer) {}
 
-  bitrates!: BitrateOption[];
-
-  streams: IMediaStream[] = [
-    {
-      type: 'vod',
-      label: 'VOD',
-      source: 'http://static.videogular.com/assets/videos/videogular.mp4'
-    },
-    {
-      type: 'dash',
-      label: 'DASH: Multi rate Streaming',
-      source: 'http://dash.edgesuite.net/akamai/bbb_30fps/bbb_30fps.mpd'
-    },
-    {
-      type: 'dash',
-      label: 'DASH: Live Streaming',
-      source: 'https://24x7dash-i.akamaihd.net/dash/live/900080/dash-demo/dash.mpd'
-    },
-    {
-      type: 'dash',
-      label: 'DASH: DRM with Widevine',
-      source: 'https://storage.googleapis.com/shaka-demo-assets/angel-one-widevine/dash.mpd',
-      licenseServers: {
-        'com.widevine.alpha': {
-          serverURL: 'https://widevine-proxy.appspot.com/proxy'
-        }
-      }
-    },
-    {
-      type: 'hls',
-      label: 'HLS: Streaming',
-      source: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
-    }
-  ];
-
-  constructor(private fileUploadService: FileUploadService) {}
-
-  onPlayerReady(api: VgAPI): void {
-    this.api = api;
-  }
-
-  ngOnInit(): void {
-    this.currentStream = this.streams[0];
-  }
-
-  setBitrate(option: BitrateOption): void {
-    switch (this.currentStream.type) {
-      case 'dash':
-        this.vgDash.setBitrate(option);
-        break;
-
-      case 'hls':
-        this.vgHls.setBitrate(option);
-        break;
-    }
-  }
-
-  onClickStream(stream: IMediaStream): void {
-    this.api.pause();
-    this.bitrates = [];
-
-    /*
-    const timer = Observable.timer.create(0, 10);
-    timer.subscribe(
-        () => {
-            this.currentStream = stream;
-            timer.unsubscribe();
-        }
-    );
-    */
-  }
+  ngOnInit(): void {}
 
   getVideo(): void {
-    this.fileUploadService.getFile().subscribe();
+    this.fileUploadService.getFile().subscribe(data => {
+      const objectUrl = URL.createObjectURL(data.body);
+      this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+    });
   }
 }
--- src/main/webapp/app/services/file-upload.service.ts
+++ src/main/webapp/app/services/file-upload.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from 'app/app.constants';
-import { HttpClient, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private domSanitizer: DomSanitizer) {}
 
   pushFileStorage(file: File): Observable<any> {
     const data: FormData = new FormData();
     data.append('file', file);
-    return this.http.post(SERVER_API_URL + '/', data);
+    return this.http.post(SERVER_API_URL + '/api/uploadFile', data);
   }
 
-  getFile(): Observable<any> {
-    const data: FormData = new FormData();
-    data.append('file', 'test');
-    return this.http.get(SERVER_API_URL + '/api/loadVideo');
+  getFile(): Observable<HttpResponse<Blob>> {
+    return this.http.get(SERVER_API_URL + '/api/loadVideo?file=sdsds', {
+      observe: 'response',
+      responseType: 'blob'
+    });
   }
 }
