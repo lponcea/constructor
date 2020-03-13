0d9bf64f36108da07ca93b81c892ec38b41d6d4d
jam9516 <joaquin111695@gmail.com>
2020-03-13 15:50:36 -0600

    Cambio video return 1 para busqueda si resultado.

--- src/main/webapp/app/entities/curso/curso.module.ts
+++ src/main/webapp/app/entities/curso/curso.module.ts
@@ -3,13 +3,12 @@ import { RouterModule } from '@angular/router';
 
 import { ConstructorSharedModule } from 'app/shared/shared.module';
 import { CursoDetailComponent } from './curso-detail.component';
-import { CursoUpdateComponent } from './curso-update.component';
 import { CursoDeleteDialogComponent } from './curso-delete-dialog.component';
 import { cursoRoute } from './curso.route';
 
 @NgModule({
   imports: [ConstructorSharedModule, RouterModule.forChild(cursoRoute)],
-  declarations: [CursoDetailComponent, CursoUpdateComponent, CursoDeleteDialogComponent],
+  declarations: [CursoDetailComponent, CursoDeleteDialogComponent],
   entryComponents: [CursoDeleteDialogComponent]
 })
 export class ConstructorCursoModule {}
