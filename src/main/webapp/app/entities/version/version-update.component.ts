f123a7fea0a877043fc79cdace618684a2e3ec0e
Carlos Madrid <dev.cmadrid@gmail.com>
2020-03-31 12:23:50 -0600

    FIX: Se eliminó variable sin utilizar generada por jHipster.

--- src/main/webapp/app/entities/version/version-update.component.ts
+++ src/main/webapp/app/entities/version/version-update.component.ts
@@ -4,7 +4,6 @@ import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
 
 import { IVersion, Version } from 'app/shared/model/version.model';
 import { VersionService } from './version.service';
