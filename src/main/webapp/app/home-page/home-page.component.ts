9f4375e937d3242b2f4608850ae34eccbd95f50e
Carlos Madrid <dev.cmadrid@gmail.com>
2020-04-15 12:37:32 -0500

    TEST: Se agregó ejemplo de prueba de manipulación de elementos con evento scroll.

--- src/main/webapp/app/home-page/home-page.component.ts
+++ src/main/webapp/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'jhi-home-page',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  changeBoxLeft = 0;
+  color1 = 255;
+  color2 = 0;
+  color3 = 0;
+  opacity = 0;
+  background = 'rgba(' + this.color1 + ', ' + this.color2 + ', ' + this.color3 + ', ' + this.opacity + ')';
+
   constructor() {}
+
+  @HostListener('window:scroll', ['$event']) onScroll($event: Event): void {
+    const porcentaje = (document.documentElement.scrollTop * 100) / document.documentElement.scrollHeight;
+    this.changeBoxLeft = porcentaje;
+    this.opacity = porcentaje / 100;
+    this.color1 = 255 - (255 * porcentaje) / 100;
+    this.color3 = (255 * porcentaje) / 100;
+    this.background = 'rgba(' + this.color1 + ', ' + this.color2 + ', ' + this.color3 + ', ' + this.opacity + ')';
+  }
 }
