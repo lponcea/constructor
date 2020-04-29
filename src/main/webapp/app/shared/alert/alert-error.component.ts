a9f9f0a340f46ed8d79a07fd0441cb77e6c195d6
Carlos Madrid <dev.cmadrid@gmail.com>
2020-04-29 13:29:54 -0500

    ADD: Se creó plantilla HTML dedicada para componente de alertas.

--- src/main/webapp/app/shared/alert/alert-error.component.ts
+++ src/main/webapp/app/shared/alert/alert-error.component.ts
@@ -8,15 +8,7 @@ import { AlertError } from './alert-error.model';
 
 @Component({
   selector: 'jhi-alert-error',
-  template: `
-    <div class="alerts" role="alert">
-      <div *ngFor="let alert of alerts" [ngClass]="setClasses(alert)">
-        <ngb-alert *ngIf="alert && alert.type && alert.msg" [type]="alert.type" (close)="alert.close(alerts)">
-          <pre [innerHTML]="alert.msg"></pre>
-        </ngb-alert>
-      </div>
-    </div>
-  `
+  templateUrl: './alert-error.component.html'
 })
 export class AlertErrorComponent implements OnDestroy {
   alerts: JhiAlert[] = [];
