12e6b066a0ed3d021323d715f5650765e75bb1b5
Carlos Madrid <dev.cmadrid@gmail.com>
2020-05-04 15:41:17 -0500

    TEMP: Se actualiza leyenda de "seguridad de contraseña" manualmente de manera temporal.

--- src/main/webapp/app/account/password/password-strength-bar.component.ts
+++ src/main/webapp/app/account/password/password-strength-bar.component.ts
@@ -4,7 +4,7 @@ import { Component, ElementRef, Input, Renderer } from '@angular/core';
   selector: 'jhi-password-strength-bar',
   template: `
     <div id="strength">
-      <small jhiTranslate="global.messages.validate.newpassword.strength">Password strength:</small>
+      <small jhiTranslate="global.messages.validate.newpassword.strength">Seguridad de la contraseña:</small>
       <ul id="strengthBar">
         <li class="point"></li>
         <li class="point"></li>
