92e63023911632b2cfbf4e37b71d16c6303ea0c0
AxelGut <axel.g.guevara@gmail.com>
2020-05-15 17:34:47 -0500

    Style: Se integra bloque de contenido 3 y 4.

--- src/main/webapp/app/constructor/content-blocks/content-block4/content-block4.component.ts
+++ src/main/webapp/app/constructor/content-blocks/content-block4/content-block4.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-block4.component.scss']
 })
 export class ContentBlock4Component implements OnInit {
+  imgSrc = './../../../../content/images/cover_upload.png';
+
   constructor() {}
 
   ngOnInit(): void {}
