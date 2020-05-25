f1eba0ad08b3d9e7b0cce9b033d3e3a052097de7
Carlos Madrid <dev.cmadrid@gmail.com>
2020-05-25 17:58:50 -0500

    FEAT: Se agregó servicio para mostrar título y portada en layout.

--- src/main/webapp/app/constructor/constructor-book-information/constructor-book-information.component.ts
+++ src/main/webapp/app/constructor/constructor-book-information/constructor-book-information.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentCourseService } from 'app/services/current-course.service';
+import { ICurso } from 'app/shared/model/curso.model';
+import { Subscription } from 'rxjs';
+import { FileUploadService } from 'app/services/file-upload.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'jhi-constructor-book-information',
@@ -6,7 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./constructor-book-information.component.scss']
 })
 export class ConstructorBookInformationComponent implements OnInit {
-  constructor() {}
+  subscription: Subscription;
+  course?: ICurso;
+  portadaUrl?: SafeUrl;
+
+  constructor(
+    private currentCourseService: CurrentCourseService,
+    private fileUploadService: FileUploadService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.subscription = this.currentCourseService.getCurrentCourse().subscribe(currentCourse => {
+      this.course = currentCourse;
+      if (this.course.portadaUrl) {
+        this.getCover(this.course.portadaUrl);
+      }
+    });
+  }
 
   ngOnInit(): void {}
+
+  private getCover(path: string): void {
+    this.fileUploadService.getFile(path).subscribe(data => {
+      const coverPath = URL.createObjectURL(data.body);
+      const objectUrl = this.sanitizer.bypassSecurityTrustUrl(coverPath);
+      this.portadaUrl = objectUrl;
+    });
+  }
 }
