bbc6ff8c52c74b018388d0c78fc1afc7cae26320
Carlos Madrid <dev.cmadrid@gmail.com>
2020-05-28 19:23:13 -0500

    FIX: Se corrigió error de bloque 3 y se reestructuró bloque 4 (ordenamiento provisional de componentes).

--- src/main/webapp/app/constructor/content-blocks/content-block4/content-block4.component.ts
+++ src/main/webapp/app/constructor/content-blocks/content-block4/content-block4.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { BloqueComponentes } from 'app/shared/model/bloque-componentes.model';
+import { IComponente } from 'app/shared/model/componente.model';
 
 @Component({
   selector: 'jhi-content-block4',
@@ -19,5 +20,18 @@ export class ContentBlock4Component {
       type: $event['type'],
       componentIndex: index
     });
+  }
+
+  orderTextImage(components: IComponente[]): IComponente[] {
+    let temp: IComponente[] = [];
+    if (components.length > 1) {
+      if (components[0].tipoComponente!.nombre === 'image') {
+        temp.push(components[1]);
+        temp.push(components[0]);
+      } else {
+        temp = components;
+      }
+    }
+    return temp;
   }
 }
